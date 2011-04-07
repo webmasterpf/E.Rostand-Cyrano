09ef7725f435e57854ac94076c438de4a35e8825
webmasterpf-DG <webmaster@provence-formation.com>
2011-04-07 13:38:25 +0200

    Ajout des fichiers de depart Signed-off-by: webmasterpf-DG <webmaster@provence-formation.com>

---
+++ README.txt
@@ -0,0 +1,25 @@
+9 Fev 2010
+
+ABOUT CYRANO_PF
+-----------
+
+The CYRANO_PF theme is a custom Drupal theme designed by Agence Cyrano for Association Provence Formation.
+Drupal integration is made by the Association Provence Formation's webmaster.
+This theme is made for Drupal 6.
+
+REGLAGES A FAIRE
+----------------
+
+dans template.php :
+remplacer le nom du theme dans les fonctions nécessaires.
+
+Changer le nom du fichier .info
+
+Changer le favicon
+Changer le screenshot
+Changer le logo
+
+Dans images
+-----------
+
+Mettre à jour les images du thème
---
+++ js/code_stats.php
@@ -0,0 +1,58 @@
+<!--  ######## CODE STATS OK !!!!!!!!!!!!-->
+<!-- code XITI  -->
+<div id="xiti-logo" style="visibility: hidden;">
+<script type="text/javascript">
+<!--
+document.write('<a href="http://www.xiti.com/xiti.asp?s=464402" title="WebAnalytics">');
+Xt_param = 's=464402&p=<?php echo $head_title; ?>';
+try {Xt_r = top.document.referrer;}
+catch(e) {Xt_r = document.referrer; }
+Xt_h = new Date();
+Xt_i = '<img width="39" height="25" border="0" ';
+Xt_i += 'src="http://logv11.xiti.com/hit.xiti?'+Xt_param;
+Xt_i += '&hl='+Xt_h.getHours()+'x'+Xt_h.getMinutes()+'x'+Xt_h.getSeconds();
+if(parseFloat(navigator.appVersion)>=4)
+{Xt_s=screen;Xt_i+='&r='+Xt_s.width+'x'+Xt_s.height+'x'+Xt_s.pixelDepth+'x'+Xt_s.colorDepth;}
+document.write(Xt_i+'&ref='+Xt_r.replace(/[<>"]/g, '').replace(/&/g, '$')+'"></a>');
+//-->
+</script>
+<noscript>
+<div id="xiti-logo-noscript">
+<a href="http://www.xiti.com/xiti.asp?s=455258" title="WebAnalytics" >WebAnalytics solution by <img width="39" height="25" src="http://logv11.xiti.com/hit.xiti?s=464402&amp;p=<?php echo $url; ?>&amp;" alt="WebAnalytics" /></a>
+</div>
+</noscript>
+</div>
+<!-- Start Quantcast tag -->
+<script type="text/javascript">
+_qoptions={
+qacct:"p-behZUd_cXqsz-"
+};
+</script>
+<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
+<noscript>
+<img src="http://pixel.quantserve.com/pixel/p-behZUd_cXqsz-.gif" style="display: none;" border="0" height="1" width="1" alt="Quantcast"/>
+</noscript>
+<!-- End Quantcast tag -->
+<!-- Woopra Code Start -->
+<script type="text/javascript" src="//static.woopra.com/js/woopra.v2.js"></script>
+<script type="text/javascript">
+woopraTracker.setDomain("lycee-edmond-rostand.fr");
+woopraTracker.track();
+</script>
+<!-- Woopra Code End -->
+<!-- Start of StatCounter Code -->
+<script type="text/javascript">
+var sc_project=6262185;
+var sc_invisible=1;
+var sc_security="4bc70a93";
+</script>
+
+<script type="text/javascript"
+src="http://www.statcounter.com/counter/counter_xhtml.js"></script><noscript><div
+class="statcounter"><a title="web counter"
+class="statcounter" href="http://statcounter.com/"><img
+class="statcounter"
+src="http://c.statcounter.com/6262185/0/4bc70a93/1/"
+alt="web counter" /></a></div></noscript>
+<!-- End of StatCounter Code -->
+
