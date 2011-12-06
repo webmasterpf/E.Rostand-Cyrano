b8e695458796d63e73cb1360807f2a8210064b13
webmasterpf-DG <webmaster@provence-formation.com>
2011-12-06 17:17:36 +0100

    DEV: tpl pour page classe + vue , fin page actu globale
    Signed-off-by: webmasterpf-DG <webmaster@provence-formation.com>

--- page-lycee.tpl.php
+++ page-lycee.tpl.php
@@ -2,42 +2,34 @@
 global $theme_path;
 include ($theme_path.'/includes/inc_header.php');
 ?>
- <!-- ______________________ LAYOUT PAGE BASIQUE _______________________ -->
+ <!-- ______________________ LAYOUT PAGE LYCEE OVERRIDE PAR NODE.TPL CUSTOM _______________________ -->
   <!-- ______________________ CONTENU _______________________ -->
-  
+
       <div id="contentPage">
-      
+
           <!-- ______________________ CONTENT TOP _______________________ -->
-      <?php if ($breadcrumb ||$content_top ): ?>
+      <?php if ($breadcrumb ||$content_top): ?>
             <div id="content-top">
 	<span id="ariane"> <?php print $breadcrumb; ?></span>
 
               <?php print $content_top; ?>
             </div> <!-- /#content-top -->
-	  
-      
-       <!-- ______________________ COLONNE GAUCHE _______________________ -->
-		
-			  
-         <div id="left-content">
-              <?php if ($title): ?>
-                <h1 class="title"><?php print $title; ?></h1>
-              <?php endif; ?>
-			  <?php if ($left): ?>
-            <?php print $left; ?>
-			<?php endif; ?> 
-          </div>
-             <!-- /sidebar-left -->
-		
-		<!--fin du contenu gauche -->
-        
-        <div id="content-inner" class="inner column center">
-		             
-		
-           
+            <?php endif; ?>
+
+  <!-- ______________________ CONTENT TOP NODE_______________________ -->
+               <?php if ($content_top_node): ?>
+            <div id="content-top-node">
+	              <?php print $content_top_node; ?>
+            </div> <!-- /#content-top-node -->
+            <?php endif; ?>
+
+       <!-- ______________________ CONTENT INNER GLOBAL _______________________ -->
+
+
+        <div id="content-inner-page-MODELE">
 
           <?php if ($mission || $messages || $help || $tabs): ?>
-            <div id="content-header">              
+            <div id="content-header">
 
               <?php if ($mission): ?>
                 <div id="mission"><?php print $mission; ?></div>
@@ -45,7 +37,7 @@ include ($theme_path.'/includes/inc_header.php');
 
               <?php print $messages; ?>
 
-              <?php print $help; ?> 
+              <?php print $help; ?>
 
               <?php if ($tabs): ?>
                 <div class="tabs"><?php print $tabs; ?></div>
@@ -54,19 +46,19 @@ include ($theme_path.'/includes/inc_header.php');
             </div> <!-- /#content-header -->
           <?php endif; ?>
 		  <!-- ______________________ CONTENU CENTRAL _______________________ -->
-          <div id="middle-content">
-          <pre> <?php //print_r ($node); ?> </pre>   <!-- listage des variables du $content -->
+          <div id="middle-content-page-MODELE">
+
             <?php print $content; ?>
               <?php print $feed_icons; ?>
           </div> <!-- /#content-area -->
 
-        
 
-          
-          
+
+
+
       </div> <!-- /content-inner /content -->
 
-            
+
         <?php if (!empty($primary_links) or !empty($secondary_links)): ?>
           <div id="navigation" class="menu <?php if (!empty($primary_links)) { print "with-main-menu"; } if (!empty($secondary_links)) { print " with-sub-menu"; } ?>">
             <?php if (!empty($primary_links)){ print theme('links', $primary_links, array('id' => 'primary', 'class' => 'links main-menu')); } ?>
@@ -74,18 +66,9 @@ include ($theme_path.'/includes/inc_header.php');
           </div> <!-- /navigation -->
         <?php endif; ?>
 
-       
-		  <!-- ______________________ COLONNE DROITE _______________________ -->
-        <?php if ($right): ?>
-         <!--d�but du contenu droit -->
-		<div id="right-content">
-			
-				
-            <?php print $right; ?>
-        </div>
-        <?php endif; ?> <!-- /sidebar-right -->
 
-   
+
+
     	 <br clear="all"/>
          <!-- ______________________ CONTENU BAS _______________________ -->
 <?php if ($content_bottom): ?>
@@ -94,10 +77,7 @@ include ($theme_path.'/includes/inc_header.php');
             </div><!-- /#content-bottom -->
           <?php endif; ?>
 	 </div> <!-- /contentPage -->
-	  
-	<?php
+<?php
 global $theme_path;
 include ($theme_path.'/includes/inc_footer.php');
-?>
-         <?php endif; ?>
-     
+?>     
---
+++ views/views-view-grid--Classes-infos--page-1.tpl.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * @file views-view-grid.tpl.php
+ * Default simple view template to display a rows in a grid.
+ *
+ * - $rows contains a nested array of rows. Each row contains an array of
+ *   columns.
+ * - $class contains the class of the table.
+ * - $attributes contains other attributes for the table.
+ * @ingroup views_templates
+ *
+ * MISE EN FORME DE LA GRILLE
+ */
+?>
+<?php
+/* Ce template permet la création d'un layout multicolonne pour les pages de base, en permettant la disposition facile
+ * des champs CCK custom, si nécessaires pour une page de base.
+*/?>
+<!--______________NODE TPL POUR PAGE.TPL CUSTOM________________ -->
+<div class="node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
+    <div class="node-inner">
+        <!--______________COLONNE 1________________ -->
+        <?php /* choix du layout selon nombre de colonne
+         * .col1_layout_200_590_200{} .col1_layout_330_all{} .col1_layout_18_56_25{} .col1_layout_730_250{}
+         * .col2_layout_200_590_200{} .col2_layout_330_all{} .col2_layout_18_56_25{} .col2_layout_730_250{}
+         * .col3_layout_200_590_200{} .col3_layout_330_all{} .col3_layout_18_56_25{}
+         * Possible 2 colonnes avec derniere option;alors supprimer colonne-3
+         */?>
+        <div id="colonne-1" class="col1_layout_730_250 plycee">
+            <?php
+
+$viewname_ci1 = 'Classes_infos';
+$view = views_get_view ($viewname_ci1);
+$view->set_display('page_1');
+
+
+//Exécution de le vue
+$view->pre_execute();
+$view->execute();
+
+if ($view->result) {
+  // Récupération du titre du display
+  $output = '<h1 class="titre_rouge">'.$view->get_title().'</h1>';
+}
+
+//Affiche la vue
+print $output;
+
+?>
+            <?php if ($title): /*copier le titre dans la colonne desirée*/?>
+            <h1 class="titre_page plycee"><?php print $title; ?></h1>
+            <?php endif; ?>
+
+               <?php print $picture; ?>
+
+            <?php if ($submitted): ?>
+            <span class="submitted"><?php print $submitted; ?></span>
+            <?php endif; ?>
+
+            <div class="content">
+
+
+<?php if (!empty($title)) : ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<table id="table-classes" class="<?php print $class; ?>"<?php print $attributes; ?>>
+  <tbody>
+    <?php foreach ($rows as $row_number => $columns): ?>
+      <?php
+        $row_class = 'row-' . ($row_number + 1);
+        if ($row_number == 0) {
+          $row_class .= ' row-first';
+        }
+        if (count($rows) == ($row_number + 1)) {
+          $row_class .= ' row-last';
+        }
+      ?>
+      <tr class="<?php print $row_class; ?>">
+        <?php foreach ($columns as $column_number => $item): ?>
+          <td class="<?php print $column_classes[$row_number][$column_number]; ?>">
+            <?php print $item; ?>
+          </td>
+        <?php endforeach; ?>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+            </div>
+
+
+             <?php
+              global $theme_path;
+              include ($theme_path.'/includes/inc_region_col_G1.php');
+              ?>
+        </div>
+        <!--______________COLONNE 2________________ -->
+         <!-- <pre> <?php //print_r($node); ?> </pre>-->   <!-- listage des variables du $content -->
+        <div id="colonne-2" class="col2_layout_730_250 plycee">
+
+
+ <?php
+              global $theme_path;
+              include ($theme_path.'/includes/inc_rostand_actus.php');
+              ?>
+
+        </div>
+
+
+            <!--______________LIENS MENU et TAXO________________ -->
+        <?php if ($terms): ?>
+        <div class="taxonomy"><?php //print $terms; ?></div>
+        <?php endif;?>
+
+        <?php if ($links): ?>
+        <div class="links"> <?php //print $links; ?></div>
+        <?php endif; ?>
+
+    </div> <!-- /node-inner -->
+</div> <!-- /node-->
